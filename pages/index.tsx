bc234ad570355befa92292c6506a9dc5625758c9
Aseem Gupta <80787027+Aseem5047@users.noreply.github.com>
2023-04-04 18:07:08 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ const Home = ({
 
 	if (loading || subscription === null) return null;
 	if (loading) return <Loading />;
-	if (!subscription) return <Plans products={products} />;
+// 	if (!subscription) return <Plans products={products} />;
 
 	return (
 		<div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
